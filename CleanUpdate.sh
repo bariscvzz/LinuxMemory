bdda175a2d3abe66e16558bdbec5bc37e128bea2
Barış Ceviz <75948490+bariscvzz@users.noreply.github.com>
2022-08-19 01:22:28 +0300

    Update CleanUpdate.sh

--- CleanUpdate.sh
+++ CleanUpdate.sh
@@ -1,5 +1,11 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
+#!/bin/bash
+echo "Please wait...Update"
+sudo apt-get update -y
+echo "Please wait...Upgrade"
+sudo apt-get upgrade -y
+echo "Please wait...Dist-Upgrade"
+sudo apt-get dist-upgrade -y
+echo "Please wait...Autoremove" 
+sudo apt-get autoremove -y
+echo "Please wait...Autoclean"
+sudo apt-get autoclean -y
